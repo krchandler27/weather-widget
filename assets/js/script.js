85acfd244cc12dd70819e44ac36c3a892cb7afea
krchandler27 <krchandler27@gmail.com>
2022-12-17 18:43:10 -0700

    still working on fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,26 @@
 var APIKey = '6d390e6f004929169858f273e5940521'
-var city = document.getElementById('searchBox').textContent;
-var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+var city;
+var queryURL;
 var submitBtn = document.getElementById('submitBtn');
 
-function getAPI(queryURL) {
-    var city = document.getElementById('searchBox').textContent;
-    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+function getAPI(event) {
+    city = document.getElementById('searchBox').value;
+    queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial'
+    event.preventDefault();
     fetch(queryURL)
         .then(function (response) {
             console.log(response);
             return response.json();
         })
         .then(function (data) {
-            var myDiv = document.createElement('div');
-            myDiv.setAttribute("id", "date");
-            myDiv.classList.add("weather-forecast");
-            myDiv.textContent = data.current.dt;
-            document.querySelector("#weather-box").appendChild(myDiv);
             console.log(data);
+            var myDiv = document.createElement('div');
+            myDiv.setAttribute('id', 'date');
+            myDiv.classList.add('weather-forecast');
+            myDiv.textContent = data.main.temp;
+            document.querySelector('#weather-box'.appendChild(myDiv));
         })
 
 }
 
-submitBtn.addEventListener("click", getAPI(queryURL));
+submitBtn.addEventListener('click', getAPI);
