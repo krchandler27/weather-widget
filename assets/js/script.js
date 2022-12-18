05fbef58735d09f5268a2e21e7dd5cf1aaa5cc84
krchandler27 <krchandler27@gmail.com>
2022-12-17 17:28:09 -0700

    working on the getAPI function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var APIKey = '6d390e6f004929169858f273e5940521'
-var city = document.getElementById('search').textContent;
+var city = document.getElementById('searchBox').textContent;
 var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+var submitBtn = document.getElementById('submitBtn');
 
 function getAPI(queryURL) {
+    var city = document.getElementById('searchBox').textContent;
+    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
     fetch(queryURL)
         .then(function (response) {
             console.log(response);
@@ -12,11 +15,11 @@ function getAPI(queryURL) {
             var myDiv = document.createElement('div');
             myDiv.setAttribute("id", "date");
             myDiv.classList.add("weather-forecast");
-            myDiv.textContent = data.date;
+            myDiv.textContent = data.current.dt;
             document.querySelector("#weather-box").appendChild(myDiv);
             console.log(data);
         })
 
 }
 
-getAPI(queryURL);
+submitBtn.addEventListener("click", getAPI(queryURL));
