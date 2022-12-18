c3d0474850cae4410690596d1ff22f150b0cd1fa
krchandler27 <krchandler27@gmail.com>
2022-12-18 14:40:52 -0700

    updated code to hide and unhide info, also for the getAPI function to run on pressing enter, also added capitalization of first letter in city

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,20 @@ var APIKey = '6d390e6f004929169858f273e5940521'
 var city;
 var queryURL;
 var submitBtn = document.getElementById('submitBtn');
+var cityName = document.getElementById('cityName');
+var temp = document.getElementById('temp');
+var wind = document.getElementById('wind');
+var humidity = document.getElementById('humidity');
+var searchBox = document.getElementById('searchBox');
+var date = document.getElementById('date');
+var weatherIcon = document.getElementById('weather-icon');
 
 function getAPI(event) {
     city = document.getElementById('searchBox').value;
     queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial'
     event.preventDefault();
+    var CAPcity = city.charAt(0).toUpperCase() + city.slice(1);
+
     fetch(queryURL)
         .then(function (response) {
             console.log(response);
@@ -14,13 +23,24 @@ function getAPI(event) {
         })
         .then(function (data) {
             console.log(data);
-            var myDiv = document.createElement('div');
-            myDiv.setAttribute('id', 'date');
-            myDiv.classList.add('weather-forecast');
-            myDiv.textContent = data.main.temp;
-            document.querySelector('#weather-box'.appendChild(myDiv));
+            document.querySelector('.date1').classList.remove('hide');
+            document.querySelector('.temperature').classList.remove('hide');
+            document.querySelector('.windSpeed').classList.remove('hide');
+            document.querySelector('.humidity1').classList.remove('hide');
+            cityName.textContent = CAPcity;
+            weatherIcon.textContent = data.weather[0].main;
+            temp.textContent = data.main.temp;
+            wind.textContent = data.wind.speed;
+            humidity.textContent = data.main.humidity;
+            date.textContent = dayjs().format('MM/DD/YYYY');
         })
-
 }
 
 submitBtn.addEventListener('click', getAPI);
+
+searchBox.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById('submitBtn').click();
+    }
+});
